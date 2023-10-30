73c1fc75d254188df2969fe9d96deacf10e507c9
Dudevss <patinyatiger1223@gmail.com>
2023-10-30 16:28:24 +0700

    Create data product and methods addToCart,getItems,clearCart

---
+++ src/app/services/cart/cart.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/Api/products'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+
+// Methods
+  addToCart(product: Product){
+    this.items.push(product);
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  clearCart(){
+    this.items = [];
+    return this.items
+  }
+}
