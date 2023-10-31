70b6306e157e43085e8bac2968c8e83c18e7ef8f
Dudevss <patinyatiger1223@gmail.com>
2023-10-31 09:58:31 +0700

    Import CartService create items-cartService , constructor-cartService

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  items = this.cartService.getItems();
 
+  constructor(private cartService: CartService) {}
 }
