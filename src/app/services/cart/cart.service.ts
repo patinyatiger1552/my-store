68816d31d7ab5b63c4fa693d7008fd69054ed0c5
Dudevss <patinyatiger1223@gmail.com>
2023-10-31 10:10:46 +0700

    Import HttpClient , add constructor-HttpClient create methods 'getShippingPrices'

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Product } from 'src/app/Api/products'
+import { Product } from 'src/app/Api/products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
 
-// Methods
-  addToCart(product: Product){
+  constructor(private http: HttpClient) {}
+
+  // Methods
+  addToCart(product: Product) {
     this.items.push(product);
   }
 
-  getItems(){
+  getItems() {
     return this.items;
   }
 
-  clearCart(){
+  clearCart() {
     this.items = [];
-    return this.items
+    return this.items;
+  }
+
+  // function getShippingPrices
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>
+    ('/assets/shipping.json')
   }
 }
